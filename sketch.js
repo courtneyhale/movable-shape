d6d45f07fee15b2ae5cc7030a400c20db913d2ef
courtneyhale <courtneyhale44@gmail.com>
2016-02-04 16:44:38 -0500

    Inputting Girl Code

--- sketch.js
+++ sketch.js
@@ -6,7 +6,34 @@ var myShape = movableShape;
 // (Basically, you can copy and paste your code from last time, although you may wish to refactor it so that it's more readable.)
 // That said, you may wish to try putting your unrefactored, raw drawing code from week 1 in here
 myShape.drawShape = function () {
-	// your code goes here
+	function setup() {
+		createCanvas(1000,1000)
+		background('yellow');//yellow background
+		line(200,400,350,500);//left arm
+		line(600,400,400,540);//right arm
+		line(350,700,350,800);//left leg
+		line(450,700,450,800);//right leg
+		fill('blue');//blue feet
+		ellipse(310,800,90,30);//left foot
+		ellipse(490,800,90,30);//right foot
+		fill('pink');//pink dress
+		triangle(400,400,250,700,550,700);//girl dress
+		fill('white');//white face
+		ellipse(400,360,180,180);//head
+		ellipse(360,340,20,20);//left eye
+		ellipse(440,340,20,20);//right eye
+		fill('black');// black eyeball
+		ellipse(360,342,12,17);//left eyeball
+		ellipse(440,342,12,17);//right eyeball
+		line(370,400,420,400);//smile
+
+
+
+	}
+
+	function draw() {
+
+	}// your code goes here
 };
 
 // You will also need to do some work to set the speed of the shape. How do you want it to move? What do you need to change to get it to move?
