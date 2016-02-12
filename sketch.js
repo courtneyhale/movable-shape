c3117061cf7548ae97a4994a03ae1e27fb8c0aa6
courtneyhale <courtneyhale44@gmail.com>
2016-02-11 21:58:25 -0500

    Attempting speed

--- sketch.js
+++ sketch.js
@@ -1,49 +1,37 @@
 // give movableShape a nice, descriptive name to work with here
 // (i.e., replace the variable name myShape with something more descriptive (e.g., myCar, myRocket).)
-var myShape = movableShape;
+var myGirl = movableShape;
 
 // Redefine drawShape() by putting your moving shape in here
 // (Basically, you can copy and paste your code from last time, although you may wish to refactor it so that it's more readable.)
 // That said, you may wish to try putting your unrefactored, raw drawing code from week 1 in here
-myShape.drawShape = function () {
-	function setup() {
-		createCanvas(1000,1000)
-		background('yellow');//yellow background
-		line(200,400,350,500);//left arm
-		line(600,400,400,540);//right arm
-		line(350,700,350,800);//left leg
-		line(450,700,450,800);//right leg
-		fill('blue');//blue feet
-		ellipse(310,800,90,30);//left foot
-		ellipse(490,800,90,30);//right foot
-		fill('pink');//pink dress
-		triangle(400,400,250,700,550,700);//girl dress
-		fill('white');//white face
-		ellipse(400,360,180,180);//head
-		ellipse(360,340,20,20);//left eye
-		ellipse(440,340,20,20);//right eye
-		fill('black');// black eyeball
-		ellipse(360,342,12,17);//left eyeball
-		ellipse(440,342,12,17);//right eyeball
-		line(370,400,420,400);//smile
+myGirl.drawShape = function () {
+	createCanvas(1000,1000)
+	background('yellow');//yellow background
+	line(200,400,350,500);//left arm
+	line(600,400,400,540);//right arm
+	line(350,700,350,800);//left leg
+	line(450,700,450,800);//right leg
+	fill('blue');//blue feet
+	ellipse(310,800,90,30);//left foot
+	ellipse(490,800,90,30);//right foot
+	fill('pink');//pink dress
+	triangle(400,400,250,700,550,700);//girl dress
+	fill('white');//white face
+	ellipse(400,360,180,180);//head
+	ellipse(360,340,20,20);//left eye
+	ellipse(440,340,20,20);//right eye
+	fill('black');// black eyeball
+	ellipse(360,342,12,17);//left eyeball
+	ellipse(440,342,12,17);//right eyeball
+	line(370,400,420,400);//smile
 
-
-
-	}
-
-	function draw() {
-
-	}// your code goes here
-};
-
-// You will also need to do some work to set the speed of the shape. How do you want it to move? What do you need to change to get it to move?
-
-setup = function() {
-	createCanvas(600, 600);
 };
 
 draw = function() {
   background(100); // refresh the background
-  myShape.display(); // display myShape
-  myShape.update(); // and then update it
+	myGirl.x = 0;
+	myGirl.speed.x = 1;
+  myGirl.display(); // display myShape
+  myGirl.update(); // and then update it
 };
